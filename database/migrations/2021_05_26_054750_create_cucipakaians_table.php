c3133bb84adbb995bbd9a9c617a1a587b21ddaff
ghiffary1 <ghiffaryrabbani@gmail.com>
2021-05-27 20:26:21 +0700

    perbaikan db pakaian

--- database/migrations/2021_05_26_054750_create_cucipakaians_table.php
+++ database/migrations/2021_05_26_054750_create_cucipakaians_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCucipakaiantTable extends Migration
+class CreateCucipakaiansTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCucipakaiantTable extends Migration
      */
     public function up()
     {
-        Schema::create('cucipakaiant', function (Blueprint $table) {
+        Schema::create('cucipakaians', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('nname');
             $table->string('nohp');
@@ -30,6 +30,6 @@ class CreateCucipakaiantTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cucipakaiant');
+        Schema::dropIfExists('cucipakaians');
     }
 }
