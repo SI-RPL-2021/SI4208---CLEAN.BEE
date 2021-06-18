0e195dd4c1b7561df23964104878e6060f96c9f3
Anisah Fredella <anshfrdlla@gmail.com>
2021-06-18 20:37:52 +0700

    margin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -277,7 +277,7 @@
                     <div class="topbar-divider d-none d-sm-block"></div> --> 
 
                     <!-- Nav Item - User Information -->
-                    <li style="margin-left:1000px;" class="nav-item dropdown no-arrow">
+                    <li style="margin-left:900px;" class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
                             <figure class="img-profile rounded-circle avatar font-weight-bold" data-initial="{{ Auth::user()->name[0] }}"></figure>
